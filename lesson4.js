8f88adf16170b52730109ca7995c038ec03ebf0f
NORBiNikosh <gokoki21010@gmail.com>
2024-03-30 20:28:55 +0200

    lesson4.js

---
+++ lesson4.js
@@ -0,0 +1,54 @@
+// * Первое задание * // 
+
+// function makeNegative(num) {
+//     if (num <= 0) {
+//       return num;
+//     } else {
+//       return num * -1;
+//     }
+//   };
+  
+//   console.log(makeNegative(10));
+
+// * Второе задание * // 
+
+//     function play(player, computer){
+//         if (player === computer) {
+//         console.log ("Ничья!");
+//         } else if (
+//         (player === "камень" && computer === "ножницы") ||
+//         (player === "ножницы" && computer === "бумага") ||
+//         (player === "бумага" && computer === "камень")
+//         ) {
+//         console.log ("Вы победили!");
+//         } else {
+//         console.log( "Компьютер победил!");
+//         }
+//     }
+// play("бумага","ножницы")
+
+// * Третье задание * // 
+
+// let arrayNumbers = [-4,12,-56,-10,-34]
+// let arrayRez = 0
+// function sumArray(array){
+//     for(let i=0;i<array.length;i++){
+//         arrayRez = arrayRez + Math.abs(array[i])
+        
+// }
+// console.log(arrayRez)
+// }
+// sumArray(arrayNumbers)
+
+// * Четвертое задание * // 
+// let MaxArray = []
+// function valueMax(step,maxValue){
+//     let max = maxValue*step
+//     for(let y=0;y<=max;y+=step){
+//         MaxArray.push(y)
+        
+//     }
+//     console.log(MaxArray)
+// }
+
+// valueMax(2,12)
